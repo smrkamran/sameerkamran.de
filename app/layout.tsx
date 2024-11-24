71f3fa7fe70ad8521caca16f37c5520ec36415b3
Sameer Kamran <sameer.kamran@schwarm.com>
2024-11-24 18:41:04 +0100

    Landing page ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,19 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGrotesk-Light.ttf",
+  variable: "--font-space-grotesk",
   weight: "100 900",
 });
 
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,129 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { BrainCircuit, Code } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
+const navBarItems = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Blog",
+    href: "/blog",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <nav className="flex items-center justify-center gap-4">
+        {navBarItems.map((item) => (
+          <Link
+            key={item.href}
+            className="text-[0.8rem] md:text-[1rem] lg:text-[1rem] tracking-widest py-1 px-2 border border-transparent rounded-sm hover:border-white"
+            href={item.href}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </nav>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="flex flex-col md:flex-row gap-8 justify-center items-center">
+          <div>
+            <Image
+              className="rounded-full filter grayscale-[70%] contrast-200 transition duration-300 hover:filter-none"
+              src="/Sameer_DP.jpg"
+              alt="Sameer Kamran Photo"
+              width={180}
+              height={38}
+              priority
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-[1.5rem] md:text-[3rem] lg:text-[4rem]  tracking-wider">
+              Sameer Kamran
+            </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <div className="flex gap-4 items-start md:items-center lg:items-center">
+              <div className="flex gap-2">
+                <Code size={20} />
+                <p className="text-[0.9rem] md:text-[0.9rem] ld:text-[1.1rem] tracking-widest">
+                  Software Engineer
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <BrainCircuit size={20} />
+                <p className="text-[0.9rem] md:text-[0.9rem] ld:text-[1.1rem] tracking-widest">
+                  Learner
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className=""
+            href="https://www.linkedin.com/in/sameer-kamran-dev/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              aria-hidden
+              className="invert"
+              src="/linkedin.svg"
+              alt="LinkedIn icon"
+              width={25}
+              height={25}
             />
-            Deploy now
           </a>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className=""
+            href="https://x.com/smrkamran"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            <Image
+              aria-hidden
+              className="invert"
+              src="/x.svg"
+              alt="X icon"
+              width={25}
+              height={25}
+            />
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+          <a
+            className=""
+            href="https://github.com/smrkamran"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              className="invert"
+              src="/github.svg"
+              alt="Github icon"
+              width={25}
+              height={25}
+            />
+          </a>
+        </motion.div>
       </footer>
     </div>
   );
